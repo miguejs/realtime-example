35e7dde4c13ed808a8ee80be829a56c2163ac7b4
Mike <imiked.mail@gmail.com>
2017-11-29 22:18:06 -0600

    Add basic components

---
+++ src/components/Feed.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="feed-container">
+      <div class="siimple-h3">Feed</div>
+      <div class="siimple-alert siimple-alert--info">
+        Github: <span class="siimple-link"> https://github.com/ </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Feed'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+  }
+
+  .feed-container {
+    width: 50%;
+    height: auto;
+    background: white;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    padding: 50px;
+    text-align: center;
+    box-shadow: 1px 2px 5px #dbdbdb;
+
+  }
+
+  .simple-alert {
+    letter-spacing: 1.5px;
+     width: 50%;
+  }
+  .siimple-a, .siimple-link {
+    color: #fff;
+  }
+</style>
---
+++ src/components/NewLink.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <div class="form">
+      <div class="siimple-h3">Add a New Link </div>
+      <div>
+        <label class="siimple-label">Title </label>
+        <input  v-model="form.title" type="text" class="siimple-input" placeholder="Github">
+      </div>
+      <div>
+        <label class="siimple-label">URL</label>
+        <input v-model="form.url" type="text" class="siimple-input" placeholder="https://github.com/">
+      </div>
+      <div class="save-button-container">
+        <div @click="addLink" class="siimple-btn siimple-btn--blue">Add</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NewLink',
+    data () {
+      return {
+        form : {
+          title: '',
+          url: ''
+        }
+      }
+    },
+    methods: {
+      addLink: function () {
+        let link = {
+          title: this.form.title,
+          url: this.form.url
+        }
+        // we gonna replace this console log with  a socket client call
+        console.log(link)
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .form {
+    width: 300px;
+    height: auto;
+    background: white;
+    border-radius: 5px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 50px;
+    text-align: center;
+    box-shadow: 1px 2px 5px #dbdbdb;
+    position: sticky;
+    top: 0;
+  }
+
+  .siimple-input {
+    height: 35px;
+    margin: 15px;
+  }
+
+  .siimple-btn--blue {
+    background-color: #4894f0;
+    color: #fff;
+    width: 216px;
+    height: 45px;
+    margin: 15px;
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    align-items: center;
+    justify-content: space-around;
+  }
+  .save-button-container {
+    display: flex;
+    justify-content: center;
+  }
+
+</style>
